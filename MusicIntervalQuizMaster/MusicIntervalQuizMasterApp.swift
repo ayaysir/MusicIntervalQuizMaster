b52a1714a357d6cb56010523fa118e86ad462b51
윤범태 <yoonbumtae@gmail.com>
2024-09-07 17:35:01 +0900

    Initial Commit

---
+++ MusicIntervalQuizMaster/MusicIntervalQuizMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicIntervalQuizMasterApp.swift
+//  MusicIntervalQuizMaster
+//
+//  Created by 윤범태 on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MusicIntervalQuizMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
