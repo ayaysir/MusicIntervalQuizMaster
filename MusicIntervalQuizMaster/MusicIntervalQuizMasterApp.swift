4f88faad072a6d9849e01af8b9809358441082f6
윤범태 <yoonbumtae@gmail.com>
2024-11-14 20:36:32 +0900

    Delete: game center

--- MusicIntervalQuizMaster/MusicIntervalQuizMasterApp.swift
+++ MusicIntervalQuizMaster/MusicIntervalQuizMasterApp.swift
@@ -11,8 +11,7 @@ import SwiftUI
 struct MusicIntervalQuizMasterApp: App {
   var body: some Scene {
     WindowGroup {
-      // ContentView()
-      ExampleViewControllerRepresentable()
+      ContentView()
     }
   }
 }
